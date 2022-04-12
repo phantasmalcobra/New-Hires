8ee423416b68e94b5eb001ef13243309780aabf3
phantasmalcobra <102312741+phantasmalcobra@users.noreply.github.com>
2022-04-11 17:41:48 -0700

    Add files via upload

---
+++ indexpr.py
@@ -0,0 +1,42 @@
+#index practice
+
+new_member = "False"
+answ = "False"
+new_hire = "anon"
+rep = "False"
+repdemp = ""
+
+team_1 = ["Mary", "Joe", "Sue", "Caroline", "Bob"]
+
+print(team_1)
+
+answ = input("Is there a new hire?: ")
+
+print(answ)
+
+if answ == "True":
+  new_hire = str(input("Who is the new hire? "))
+  rep = input("Is someone bring replaced? ")
+  if rep == "True":
+      repdemp = str(input("Who is being replaced? "))
+      if repdemp == "Mary":
+          team_1.pop(0)
+          team_1.insert(0, new_hire)
+      elif repdemp == "Joe":
+          team_1.pop(1)
+          team_1.insert(1, new_hire)
+      elif repdemp == "Sue":
+          team_1.pop(2)
+          team_1.insert(2, new_hire)
+      elif repdemp == "Caroline":
+          team_1.pop(3)
+          team_1.insert(3, new_hire)
+      elif repdemp == "Bob":
+          team_1.pop(4)
+          team_1.insert(4, new_hire)
+      else:
+          print("incorrect input")
+  else:
+      team_1.append(new_hire)
+    
+print(team_1)
